4dda79aaf572b1105aa3472c73367b0d39c9c7fa
politecat314 <55146802+politecat314@users.noreply.github.com>
2023-02-26 16:34:02 +0800

    frontal face grids and calculations

---
+++ coordinates.py
@@ -0,0 +1,83 @@
+class Point:
+    def __init__(self, x, y):
+        """
+        a point in euclidean distance graph
+        """
+        self.x = x
+        self.y = y
+    
+    def euclidean_distance(self, point):
+        """
+        calculate euclidean distance between 2 points
+        """
+        return ((self.y - point.y)**2 + (self.x - point.x)**2) ** 1/2
+    
+    def get(self, replace_x=None):
+        """
+        get euclidean coordinates (x, y) as a tuple
+        
+        if replace_x has a value, replace the x coordinate with that
+        """
+        
+        if replace_x:
+            return (replace_x, self.y)
+        else:
+            return (self.x, self.y)
+    
+    
+    def get_point(self, replace_x=None):
+        """
+        get euclidean coordinates (x, y) as a tuple
+        
+        if replace_x has a value, replace the x coordinate with that
+        """
+        if replace_x:
+            return Point(replace_x, self.y)
+        else:
+            return Point(self.x, self.y)
+        
+        
+    def __str__(self):
+        return f"x: {self.x}\ny: {self.y}"
+    
+    @staticmethod
+    def np_array_to_Point(np_array):
+        return Point(np_array[0], np_array[1])
+    
+    
+class Line:
+    def __init__(self, point_1, point_2):
+        """
+        a line in euclidean distance graph
+        The equation for the line in the form of y = mx + c is calculated
+        takes as input 2 point objects and finds the straight line that connects them
+        """
+        self.m = (point_1.y - point_2.y) / (point_1.x - point_2.x) # gradient
+        self.c = point_1.y - (self.m * point_1.x) # y intersect
+    
+    def intersection(self, line):
+        """
+        find where this line intersects with another line
+        calculation from: https://stackabuse.com/solving-systems-of-linear-equations-with-pythons-numpy/
+        returns: Point object
+        """
+        A = np.array([[1, -self.m],
+                      [1, -line.m]])
+        
+        b = np.array([self.c, line.c])
+        solution = np.linalg.solve(A, b)
+
+        y = solution[0]
+        x = solution[1]
+        
+        return Point(x, y)
+    
+    
+    def solve(self, x):
+        """
+        find the y value when x is a certain value
+        """
+        return int( self.m*x + self.c )
+    
+    def __str__(self):
+        return f"y = {self.m}x + {self.c}"
---
+++ grids.py
@@ -0,0 +1,272 @@
+import numpy as np
+import cv2
+from coordinates import Point, Line
+
+def grid_4(image, results):
+    annotated_image = image.copy()
+
+    face = results.multi_face_landmarks[0]
+    shape = image.shape
+
+    coordinates = []
+    for landmark in face.landmark:
+        x = landmark.x
+        y = landmark.y
+
+        relative_x = int(x * shape[1])
+        relative_y = int(y * shape[0])
+
+        coordinates.append([relative_x, relative_y])
+
+    cooridnates = np.array(coordinates)
+    
+    bottom_nose = Point.np_array_to_Point( coordinates[2] )
+    
+    lip_left_edge = Point.np_array_to_Point( coordinates[61] )
+    lip_right_edge = Point.np_array_to_Point( coordinates[291] )
+    
+    left_lip_top =  Point.np_array_to_Point( coordinates[37] )
+    right_lip_top =  Point.np_array_to_Point( coordinates[267] )
+    
+    bottom_lip =  Point.np_array_to_Point( coordinates[17] )
+    
+
+    # top line
+    cv2.line(annotated_image, bottom_nose.get(lip_left_edge.x), bottom_nose.get(lip_right_edge.x),(255,0,0),2)
+    top_line = Line(bottom_nose.get_point(lip_left_edge.x), bottom_nose.get_point(lip_right_edge.x))
+
+    # middle line
+    middle_line = Line(left_lip_top, right_lip_top)
+    cv2.line(annotated_image, (lip_left_edge.x, middle_line.solve(lip_left_edge.x)), (lip_right_edge.x, middle_line.solve(lip_right_edge.x)),(255,0,0),2)
+    
+    # bottom line
+    cv2.line(annotated_image, bottom_lip.get(lip_left_edge.x), bottom_lip.get(lip_right_edge.x),(255,0,0),2)
+    bottom_line = Line(bottom_lip.get_point(lip_left_edge.x), bottom_lip.get_point(lip_right_edge.x))
+
+    
+    # top left point
+    top_left_point = (lip_left_edge.x, top_line.solve(lip_left_edge.x))
+
+    # top right point
+    top_right_point = (lip_right_edge.x, top_line.solve(lip_right_edge.x))
+
+    # middle left point
+    middle_left_point = (lip_left_edge.x, middle_line.solve(lip_left_edge.x))
+
+    # middle right point
+    middle_right_point = (lip_right_edge.x, middle_line.solve(lip_right_edge.x))
+
+    # bottom left point
+    bottom_left_point = (lip_left_edge.x, bottom_line.solve(lip_left_edge.x))
+
+    # bottom right point
+    bottom_right_point = (lip_right_edge.x, bottom_line.solve(lip_right_edge.x))
+
+    # left horizontal line
+    cv2.line(annotated_image, top_left_point, bottom_left_point,(255,0,0),2)
+
+    # right horizontal line
+    cv2.line(annotated_image, top_right_point, bottom_right_point,(255,0,0),2)
+
+# #     # points
+    cv2.circle(annotated_image, top_left_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, top_right_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, middle_left_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, middle_right_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, bottom_left_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, bottom_right_point, radius=1, color=(0, 0, 255), thickness=3)
+    
+    
+#     print("top", top_right_point, "middle", middle_right_point, "bottom", bottom_right_point)
+    top_distance = middle_right_point[1] - top_right_point[1]
+    bottom_distance = bottom_right_point[1] - middle_right_point[1]
+    print("ratio:", bottom_distance/top_distance)
+    
+#     # add text of ratio
+    text_y = ((middle_right_point[1] + bottom_right_point[1]) / 2)
+    
+    cv2.putText(img=annotated_image, text=str(bottom_distance/top_distance), org=(top_right_point[0], int(text_y)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 0, 173),thickness=2)
+    
+    return annotated_image
+
+
+def grid_11(image, results):
+    annotated_image = image.copy()
+
+    face = results.multi_face_landmarks[0]
+    shape = image.shape
+
+    coordinates = []
+    for landmark in face.landmark:
+        x = landmark.x
+        y = landmark.y
+
+        relative_x = int(x * shape[1])
+        relative_y = int(y * shape[0])
+
+        coordinates.append([relative_x, relative_y])
+
+    cooridnates = np.array(coordinates)
+    
+    
+    
+    left_eyebrow_top =  Point.np_array_to_Point( coordinates[105] )
+    right_eyebrow_top =  Point.np_array_to_Point( coordinates[334] )
+
+    left_lip_top =  Point.np_array_to_Point( coordinates[37] )
+    right_lip_top =  Point.np_array_to_Point( coordinates[267] )
+
+    left_eyebrow_edge =  Point.np_array_to_Point( coordinates[156] )
+    right_eyebrow_edge =  Point.np_array_to_Point( coordinates[300] )
+
+    bottom_chin =  Point.np_array_to_Point( coordinates[152] )
+    
+
+    # top line
+    cv2.line(annotated_image, left_eyebrow_top.get(left_eyebrow_edge.x), right_eyebrow_top.get(right_eyebrow_edge.x),(255,0,0),2)
+    top_line = Line(left_eyebrow_top, right_eyebrow_top)
+
+    # middle line
+    middle_line = Line(left_lip_top, right_lip_top)
+    cv2.line(annotated_image, (left_eyebrow_edge.x, middle_line.solve(left_eyebrow_edge.x)), (right_eyebrow_edge.x, middle_line.solve(right_eyebrow_edge.x)),(255,0,0),2)
+    
+    # bottom line
+    cv2.line(annotated_image, bottom_chin.get(left_eyebrow_edge.x), bottom_chin.get(right_eyebrow_edge.x),(255,0,0),2)
+    bottom_line = Line(bottom_chin.get_point(left_eyebrow_edge.x), bottom_chin.get_point(right_eyebrow_edge.x))
+
+    
+#     # top left point
+    top_left_point = (left_eyebrow_edge.x, top_line.solve(left_eyebrow_edge.x))
+
+#     # top right point
+    top_right_point = (right_eyebrow_edge.x, top_line.solve(right_eyebrow_edge.x))
+
+#     # middle left point
+    middle_left_point = (left_eyebrow_edge.x, middle_line.solve(left_eyebrow_edge.x))
+
+#     # middle right point
+    middle_right_point = (right_eyebrow_edge.x, middle_line.solve(right_eyebrow_edge.x))
+
+#     # bottom left point
+    bottom_left_point = (left_eyebrow_edge.x, bottom_line.solve(left_eyebrow_edge.x))
+
+#     # bottom right point
+    bottom_right_point = (right_eyebrow_edge.x, bottom_line.solve(right_eyebrow_edge.x))
+
+#     # left horizontal line
+    cv2.line(annotated_image, top_left_point, bottom_left_point,(255,0,0),2)
+
+#     # right horizontal line
+    cv2.line(annotated_image, top_right_point, bottom_right_point,(255,0,0),2)
+
+#     # points
+    cv2.circle(annotated_image, top_left_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, top_right_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, middle_left_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, middle_right_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, bottom_left_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, bottom_right_point, radius=1, color=(0, 0, 255), thickness=3)
+    
+    
+#     print("top", top_right_point, "middle", middle_right_point, "bottom", bottom_right_point)
+    top_distance = middle_right_point[1] - top_right_point[1]
+    bottom_distance = bottom_right_point[1] - middle_right_point[1]
+    print("ratio:", top_distance/bottom_distance)
+    
+#     # add text of ratio
+    text_y = ((top_right_point[1] + middle_right_point[1]) / 2)
+    
+    cv2.putText(img=annotated_image, text=str(top_distance/bottom_distance), org=(top_right_point[0], int(text_y)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 0, 173),thickness=2)
+    
+    return annotated_image
+
+
+def grid_15(image, results):
+    annotated_image = image.copy()
+
+    face = results.multi_face_landmarks[0]
+    shape = image.shape
+
+    coordinates = []
+    for landmark in face.landmark:
+        x = landmark.x
+        y = landmark.y
+
+        relative_x = int(x * shape[1])
+        relative_y = int(y * shape[0])
+
+        coordinates.append([relative_x, relative_y])
+
+    cooridnates = np.array(coordinates)
+    
+    
+    
+    left_eyebrow_top =  Point.np_array_to_Point( coordinates[105] )
+    right_eyebrow_top =  Point.np_array_to_Point( coordinates[334] )
+
+    left_eye_top =  Point.np_array_to_Point( coordinates[159] )
+    right_eye_top =  Point.np_array_to_Point( coordinates[386] )
+
+    left_eyebrow_edge =  Point.np_array_to_Point( coordinates[156] )
+    right_eyebrow_edge =  Point.np_array_to_Point( coordinates[300] )
+
+    left_eye_bottom =  Point.np_array_to_Point( coordinates[145] )
+    right_eye_bottom =  Point.np_array_to_Point( coordinates[374] )    
+    
+
+    # top line
+    cv2.line(annotated_image, left_eyebrow_top.get(left_eyebrow_edge.x), right_eyebrow_top.get(right_eyebrow_edge.x),(255,0,0),2)
+    top_line = Line(left_eyebrow_top, right_eyebrow_top)
+
+    # middle line
+    cv2.line(annotated_image, left_eye_top.get(left_eyebrow_edge.x), right_eye_top.get(right_eyebrow_edge.x),(255,0,0),2)
+    middle_line = Line(left_eye_top, right_eye_top)
+
+    # bottom line
+    cv2.line(annotated_image, left_eye_bottom.get(left_eyebrow_edge.x), right_eye_bottom.get(right_eyebrow_edge.x),(255,0,0),2)
+    bottom_line = Line(left_eye_bottom, right_eye_bottom)
+
+    # top left point
+    top_left_point = (left_eyebrow_edge.x, top_line.solve(left_eyebrow_edge.x))
+
+    # top right point
+    top_right_point = (right_eyebrow_edge.x, top_line.solve(right_eyebrow_edge.x))
+
+    # middle left point
+    middle_left_point = (left_eyebrow_edge.x, middle_line.solve(left_eyebrow_edge.x))
+
+    # middle right point
+    middle_right_point = (right_eyebrow_edge.x, middle_line.solve(right_eyebrow_edge.x))
+
+    # bottom left point
+    bottom_left_point = (left_eyebrow_edge.x, bottom_line.solve(left_eyebrow_edge.x))
+
+    # bottom right point
+    bottom_right_point = (right_eyebrow_edge.x, bottom_line.solve(right_eyebrow_edge.x))
+
+    # left horizontal line
+    cv2.line(annotated_image, top_left_point, bottom_left_point,(255,0,0),2)
+
+    # right horizontal line
+    cv2.line(annotated_image, top_right_point, bottom_right_point,(255,0,0),2)
+
+    # points
+    cv2.circle(annotated_image, top_left_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, top_right_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, middle_left_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, middle_right_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, bottom_left_point, radius=1, color=(0, 0, 255), thickness=3)
+    cv2.circle(annotated_image, bottom_right_point, radius=1, color=(0, 0, 255), thickness=3)
+    
+    
+    print("top", top_right_point, "middle", middle_right_point, "bottom", bottom_right_point)
+    top_distance = middle_right_point[1] - top_right_point[1]
+    bottom_distance = bottom_right_point[1] - middle_right_point[1]
+    print("ratio:", top_distance/bottom_distance)
+    
+    # add text of ratio
+    text_y = ((top_right_point[1] + middle_right_point[1]) / 2)
+    
+    cv2.putText(img=annotated_image, text=str(top_distance/bottom_distance), org=(top_right_point[0], int(text_y)), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 0, 173),thickness=2)
+    
+    return annotated_image
